1abec413f6c098e1fe42ebecbbcf7009ebcaee20
kl3366 <kl25143@glcomets.net>
2025-02-26 14:03:46 -0500

    a

---
+++ import util Scanner;.java
@@ -0,0 +1,12 @@
+import util Scanner;
+//import scanner
+
+public class program{
+
+    public static void main(String[] args){
+    Scanner scanner = newScanner(System.in);
+    
+    //prompt user to input security clearence
+    System.out.println("INPUT PASSWORD")
+    }
+}
